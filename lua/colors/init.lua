e37b7221b8b233d894bd5d2665a536d4c7628991
Li Ran <1489198873@qq.com>
2021-12-17 20:34:49 +0800

    fix theme

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -5,7 +5,10 @@ M.init = function(theme)
   if not theme then
     theme = vim.g.nvea_theme
   end
-  local base16 = require("base16")
+  local present, base16 = pcall(require,"base16")
+  if not present then
+    return print("Theme not present!")
+  end
 
   base16(base16.themes(theme), true)
   require("colors.highlights")
